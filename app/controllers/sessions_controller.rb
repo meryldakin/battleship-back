0c5ffe6aa7f31ce012718b284a521bb44fa52734
meryldakin <meryl.k.dakin@gmail.com>
2017-05-11 09:31:43 -0400

    started making the sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def index
+    if session[:user_id]
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      session[:ready] = false
+      redirect_to user_path(@user)
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to home_path
+  end
+
+end
